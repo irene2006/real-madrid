df572fbae587f582478accb2992116057865c575
irene <irenemartingarcia06122006@gmail.com>
2019-12-27 11:50:03 +0100

    primer cambio

---
+++ js/functions.js
@@ -0,0 +1,15 @@
+function cambiarimagen1() {
+    document.getElementById("paso").src = "img/PRUEBA2.PNG";
+}
+
+function cambiarimagen2() {
+    document.getElementById("paso").src = "img/COURTOIS.PNG";
+}
+
+function cambiarimagen3() {
+    document.getElementById("paso2").src = "img/informacionsergio.png";
+}
+
+function cambiarimagen4() {
+    document.getElementById("paso2").src = "img/sergio ramos info223.png";
+}
